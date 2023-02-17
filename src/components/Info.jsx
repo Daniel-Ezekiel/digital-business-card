0df0616072f8fcd3591923bbd41f78096d1a4a40
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-02-17 15:18:56 +0100

    style: increased font size of info-details

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,7 +2,7 @@ function Info({ title, details }) {
   return (
     <div className="info--container">
       <h2 className="info--title">{title}</h2>
-      <p className="info-details">{details}</p>
+      <p className="info--details">{details}</p>
     </div>
   );
 }
